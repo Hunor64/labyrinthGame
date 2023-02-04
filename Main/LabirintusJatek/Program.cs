e801986409d331d95dcf8e794bc0516df328b36c
Hunor József Nagy <Nhunor06@gmail.com>
2023-02-04 15:38:16 +0100

    Commit 1

---
+++ Main/LabirintusJatek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LabirintusJatek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int language = 1;
+            Console.Write("Válasszon nyelvet! / Choose a Language:\n1. Magyar\n2.English");
+            language = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+        }
+    }
+}
