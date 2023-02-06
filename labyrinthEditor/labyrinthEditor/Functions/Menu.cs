149e222ad0ea2f2800228713a4ee47db189ccd0c
suketti <54027485+suketti@users.noreply.github.com>
2023-02-06 14:44:05 +0100

    Add change language function

--- labyrinthEditor/labyrinthEditor/Functions/Menu.cs
+++ labyrinthEditor/labyrinthEditor/Functions/Menu.cs
@@ -17,5 +17,62 @@ namespace labyrinthEditor.Functions
             Console.WriteLine("3. " + Resources.strings.ChangeLanguage);
             Console.WriteLine("4. " + Resources.strings.ExitMenu);
         }
+
+        public static void MainMenu(Map map, CursorMovement cursorMovement) {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Menu.DisplayMenu();
+                displayMenu = false;
+                switch (Console.ReadKey(true).KeyChar) {
+                    case '1':
+                        Console.Write("\n" + Resources.strings.AskWidth + ": ");
+                        int x = Int32.Parse(Console.ReadLine());
+                        Console.Write("\n" + Resources.strings.AskHeight + ": ");
+                        int y = Int32.Parse(Console.ReadLine());
+                        map.CreateMap(y, x);
+                        map.PrintMap();
+                        cursorMovement.EnableCursorMovement(map);
+                        break;
+                    case '2':
+                        Console.WriteLine();
+                        map.LoadMap("minta.txt");
+                        map.PrintMap();
+                        cursorMovement.EnableCursorMovement(map);
+                        break;
+                    case '3':
+                        
+                        break;
+                    case '4':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        displayMenu = true;
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+
+        public static void ChangeLanguage() {
+            Console.Clear();
+            Console.WriteLine("1. Hungarian/Magyar");
+            Console.WriteLine("2. English");
+            Console.WriteLine("3. return to the menu")
+            ConsoleKeyInfo input =  Console.ReadKey(true);
+
+            switch (input.Key)
+            {
+                case ConsoleKey.D1:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("hu-HU");
+                case ConsoleKey.D2:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                case ConsoleKey.D3:
+                    
+                default:
+                    ChangeLanguage();
+            }   
+        }
     }
 }
--- labyrinthEditor/labyrinthEditor/Program.cs
+++ labyrinthEditor/labyrinthEditor/Program.cs
@@ -12,39 +12,8 @@ class Program
         Map map = new Map();
         CursorMovement cursorMovement = new CursorMovement();
         Console.OutputEncoding = Encoding.UTF8;
-        bool displayMenu = true;
-        while (displayMenu)
-        {
-            Menu.DisplayMenu();
-            displayMenu = false;
-            switch (Console.ReadKey(true).KeyChar) {
-                case '1':
-                    Console.Write("\n" + Resources.strings.AskWidth + ": ");
-                    int x = Int32.Parse(Console.ReadLine());
-                    Console.Write("\n" + Resources.strings.AskHeight + ": ");
-                    int y = Int32.Parse(Console.ReadLine());
-                    map.CreateMap(y, x);
-                    map.PrintMap();
-                    cursorMovement.EnableCursorMovement(map);
-                    break;
-                case '2':
-                    Console.WriteLine();
-                    map.LoadMap("minta.txt");
-                    map.PrintMap();
-                    cursorMovement.EnableCursorMovement(map);
-                    break;
-                case '3':
-                    Console.WriteLine("3");
-                    break;
-                case '4':
-                    Environment.Exit(0);
-                    break;
-                default:
-                    displayMenu = true;
-                    break;
-            }
-            Console.ReadKey();
-        }
+        
+        MainMenu()
     }
 
 }
